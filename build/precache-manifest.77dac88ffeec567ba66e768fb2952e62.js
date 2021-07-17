047614869d666f1c8b0279077cf4382c290ed844
Shaurya55 <87562562+Shaurya55@users.noreply.github.com>
2021-07-17 20:30:52 +0530

    Frontend

---
+++ build/precache-manifest.77dac88ffeec567ba66e768fb2952e62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2c33b8e3ee320c66ff8a2b189fb9bbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "15add050b420022276c6",
+    "url": "/static/css/main.5d4b47cf.chunk.css"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/static/js/2.4254f81d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4254f81d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15add050b420022276c6",
+    "url": "/static/js/main.4219598a.chunk.js"
+  },
+  {
+    "revision": "44d459044b9da8ae1201",
+    "url": "/static/js/runtime-main.6829a894.js"
+  }
+]);
